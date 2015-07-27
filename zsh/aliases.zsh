6f9e58bc5446ad6bd6caeebab4b4adabb7f25f12
h1-the-swan <jason.portenoy@gmail.com>
2015-07-27 10:26:34 -0700

    added the httpserver function to start a Node.js simple local server to the zsh FPATH (in aliases.zsh)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,16 @@
 alias reload!='. ~/.zshrc'
 
-# 2014-07-18 added by h1-the-swan
-alias httpserver="python -m SimpleHTTPServer"
+# 2015-07-18 added by h1-the-swan
+# alias httpserver="python -m SimpleHTTPServer"
+
+# 2015-07-27 added by h1-the-swan
+# http://unix.stackexchange.com/questions/33255/how-to-define-and-load-your-own-shell-function-in-zsh
+function httpserver() { 
+    http-server || 
+    {
+	    echo "http-server could not be run. Do you have node.js and http-server installed? Tyring Python's SimpleHTTPServer instead" ;
+	    python -m SimpleHTTPServer; 
+    }
+}
+# Add the above function to FPATH
+fpath=( httpserver "${fpath[@]}" )
