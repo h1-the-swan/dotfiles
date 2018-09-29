4e611d64d03a728900549aa5544013fc2a25d4d5
h1-the-swan <jason.portenoy@gmail.com>
2018-09-28 19:39:02 -0700

    add note to zsh prompt if running in docker container

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -71,7 +71,15 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+running_in_container() {
+  if [[ -f /.dockerenv ]]; then
+    echo " (docker)"
+  else
+    echo ""
+  fi
+}
+
+export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)$(running_in_container)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
